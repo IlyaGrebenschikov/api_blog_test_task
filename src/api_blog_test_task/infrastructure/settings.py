523d9f7cc6462ae63fb524096590981201fcdb09
IlyaGrebenschikov <ilushagr22@gmail.com>
2026-03-05 19:20:05 +0400

    fix(containers, settings): Added postgres container and DB settings

--- src/api_blog_test_task/infrastructure/settings.py
+++ src/api_blog_test_task/infrastructure/settings.py
@@ -3,8 +3,39 @@ from dataclasses import dataclass
 from typing import Optional
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from sqlalchemy import URL
 
 log = logging.getLogger(__name__)
+
+class DatabaseSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        env_prefix="DB_",
+        extra="ignore"
+    )
+    drivername: str = "postgresql+asyncpg"
+    host: str
+    port: int
+    username: str
+    password: str
+    database: str
+
+    @property
+    def url_obj(self) -> URL:
+        return URL.create(**self.model_dump())
+
+    @property
+    def url_str(self) -> str:
+        return (
+            f"{self.drivername}://"
+            f"{self.username}:"
+            f"{self.password}@"
+            f"{self.host}:"
+            f"{self.port}/"
+            f"{self.database}"
+        )
+
 
 class UvicornServerSettings(BaseSettings):
     model_config = SettingsConfigDict(
@@ -20,13 +51,20 @@ class UvicornServerSettings(BaseSettings):
 
 @dataclass
 class InfrastructureSettings:
+    database: DatabaseSettings
     server: UvicornServerSettings
 
 
+def load_database_settings(database_settings: Optional[DatabaseSettings] = None,) -> DatabaseSettings:
+    return database_settings or DatabaseSettings()
+
+
 def load_infrastructure_settings(
+        database_settings: Optional[DatabaseSettings] = None,
         server_settings: Optional[UvicornServerSettings] = None,
     ) -> InfrastructureSettings:
     log.info("Loading infrastructure settings.")
     return InfrastructureSettings(
+        database=database_settings or DatabaseSettings(),
         server=server_settings or UvicornServerSettings(),
     )
